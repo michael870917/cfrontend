617256f7af06db9161d9d7c2c629c19c73edaff9
michael870917 <michael870917@gmail.com>
2014-07-17 09:34:13 -0500

    project frontend profesional
    project frontend profesional

---
+++ index.php
@@ -0,0 +1,257 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+	<title>Armandola michel</title>
+	<link rel="stylesheet" href="css/normalize.css">
+	<link rel="stylesheet" href="css/main.css">
+</head>
+<body>
+	<header id="header" class="header">
+		<div class="logo">
+			<img src="images/logo.png" alt="Puls4">
+		</div>
+		<div class="titular">
+			<h1 class="titulo">
+				Puls4: Comunidad profesional de gente atractiva
+			</h1>
+			<div>
+				<a class="filtro" href="#">
+					Stylus
+				</a>
+				<a class="publicar" href="#">
+					Publicar
+				</a>				
+			</div>
+		</div>
+		<div class="usuario">
+			<figure class="avatar">
+				<img src="images/avatar.jpg" alt="Michael">
+			</figure>
+			<a class="flechita" href="#"></a>
+		</div>
+
+	</header><!-- /header -->
+	<nav>
+		<ul class="menu">
+			<li><a href="#">Python</a></li>
+			<li><a href="#">HTML5</a></li>
+			<li><a href="#">Javascript</a></li>
+			<li><a href="#">CSS3</a></li>
+			<li><a href="#">Django</a></li>
+
+		</ul>
+	</nav>
+	<section class="posts">
+		<article class="post">
+			<div class="descripcion">
+				<figure class="imagen">
+					<img src="images/foto.jpg">
+				</figure>
+				<div class="detalles">
+					<h2 class="titulo">
+						Colores, gradientes y texto 3D con Sass 7 Compass
+					</h2>
+					<p class="autor">
+						por <a href="#">Aldair vallejo</a>
+					</p>
+					<a class="tag">PHP</a>
+					<p class="fecha">hace <strong>20 min</strong></p>
+				</div>
+			</div>
+			<div class="acciones">
+				<div class="votos">
+					<a href="#" class="up"></a>
+					<span class="total">155</span>
+					<a href="#" class="down"></a>
+				</div>
+				<div class="datos">
+					<a class="comentarios" href="#">
+						10
+					</a>
+					<a class="estrellita" href="#">
+					</a>
+				</div>
+
+			</div>
+
+		</article>
+		<article class="post">
+			<div class="descripcion">
+				<figure class="imagen">
+					<img src="images/foto.jpg">
+				</figure>
+				<div class="detalles">
+					<h2 class="titulo">
+						Colores, gradientes y texto 3D con Sass 7 Compass
+					</h2>
+					<p class="autor">
+						por <a href="#">Aldair vallejo</a>
+					</p>
+					<a class="tag">PHP</a>
+					<p class="fecha">hace <strong>20 min</strong></p>
+				</div>
+			</div>
+			<div class="acciones">
+				<div class="votos">
+					<a href="#" class="up"></a>
+					<span class="total">155</span>
+					<a href="#" class="down"></a>
+				</div>
+				<div class="datos">
+					<a class="comentarios" href="#">
+						10
+					</a>
+					<a class="estrellita" href="#">
+					</a>
+				</div>
+
+			</div>
+
+		</article>
+		<article class="post">
+			<div class="descripcion">
+				<figure class="imagen">
+					<img src="images/foto.jpg">
+				</figure>
+				<div class="detalles">
+					<h2 class="titulo">
+						Colores, gradientes y texto 3D con Sass 7 Compass
+					</h2>
+					<p class="autor">
+						por <a href="#">Aldair vallejo</a>
+					</p>
+					<a class="tag">PHP</a>
+					<p class="fecha">hace <strong>20 min</strong></p>
+				</div>
+			</div>
+			<div class="acciones">
+				<div class="votos">
+					<a href="#" class="up"></a>
+					<span class="total">155</span>
+					<a href="#" class="down"></a>
+				</div>
+				<div class="datos">
+					<a class="comentarios" href="#">
+						10
+					</a>
+					<a class="estrellita" href="#">
+					</a>
+				</div>
+
+			</div>
+
+		</article>
+		<article class="post">
+			<div class="descripcion">
+				<figure class="imagen">
+					<img src="images/foto.jpg">
+				</figure>
+				<div class="detalles">
+					<h2 class="titulo">
+						Colores, gradientes y texto 3D con Sass 7 Compass
+					</h2>
+					<p class="autor">
+						por <a href="#">Aldair vallejo</a>
+					</p>
+					<a class="tag">PHP</a>
+					<p class="fecha">hace <strong>20 min</strong></p>
+				</div>
+			</div>
+			<div class="acciones">
+				<div class="votos">
+					<a href="#" class="up"></a>
+					<span class="total">155</span>
+					<a href="#" class="down"></a>
+				</div>
+				<div class="datos">
+					<a class="comentarios" href="#">
+						10
+					</a>
+					<a class="estrellita" href="#">
+					</a>
+				</div>
+
+			</div>
+
+		</article>
+		<article class="post">
+			<div class="descripcion">
+				<figure class="imagen">
+					<img src="images/foto.jpg">
+				</figure>
+				<div class="detalles">
+					<h2 class="titulo">
+						Colores, gradientes y texto 3D con Sass 7 Compass
+					</h2>
+					<p class="autor">
+						por <a href="#">Aldair vallejo</a>
+					</p>
+					<a class="tag">PHP</a>
+					<p class="fecha">hace <strong>20 min</strong></p>
+				</div>
+			</div>
+			<div class="acciones">
+				<div class="votos">
+					<a href="#" class="up"></a>
+					<span class="total">155</span>
+					<a href="#" class="down"></a>
+				</div>
+				<div class="datos">
+					<a class="comentarios" href="#">
+						10
+					</a>
+					<a class="estrellita" href="#">
+					</a>
+				</div>
+
+			</div>
+
+		</article>
+		<article class="post">
+			<div class="descripcion">
+				<figure class="imagen">
+					<img src="images/foto.jpg">
+				</figure>
+				<div class="detalles">
+					<h2 class="titulo">
+						Colores, gradientes y texto 3D con Sass 7 Compass
+					</h2>
+					<p class="autor">
+						por <a href="#">Aldair vallejo</a>
+					</p>
+					<a class="tag">PHP</a>
+					<p class="fecha">hace <strong>20 min</strong></p>
+				</div>
+			</div>
+			<div class="acciones">
+				<div class="votos">
+					<a href="#" class="up"></a>
+					<span class="total">155</span>
+					<a href="#" class="down"></a>
+				</div>
+				<div class="datos">
+					<a class="comentarios" href="#">
+						10
+					</a>
+					<a class="estrellita" href="#">
+					</a>
+				</div>
+
+			</div>
+
+		</article>
+	</section>
+	<footer>
+		<h3>
+			<strong>
+				Powered by
+			</strong>
+			<span class="name_autor">
+				michael vallejo	
+			</span>
+		 </h3>	
+	</footer>
+</body>
+</html>
---
+++ stylus/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+	<link rel="stylesheet" href="css/main.css">
+</head>
+<body>
+	<button class="botonera">boton 1</button>
+	<button class="botonera yellow">boton 2</button>
+	<button class="botonera gray">boton 3</button>
+
+	<div class="padre">
+		<span class="hijo">tu hijo</span>
+	</div>
+</body>
+</html>
